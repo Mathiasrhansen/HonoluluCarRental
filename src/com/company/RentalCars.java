a4a386f323ee5fab7b70371ce7b6c970002a54c5
Mathiasrhansen <math86s0@stud.kea.dk>
2021-11-10 09:41:41 +0100

    test

--- src/com/company/RentalCars.java
+++ src/com/company/RentalCars.java
@@ -4,5 +4,11 @@ public class RentalCars {
     String brand;
     String model;
     String fuelType;
-    
+    int regNum;
+    String regDate;
+    int odometer;
+
+    public  RentalCars(){
+
+    }
 }
