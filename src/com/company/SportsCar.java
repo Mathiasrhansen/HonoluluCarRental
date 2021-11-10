c8d511daed3149f6a1b91d45074f36f1848c8ac4
tonjemus <tonj0023@stud.kea.dk>
2021-11-10 10:13:37 +0100

    Update SportsCar

--- src/com/company/SportsCar.java
+++ src/com/company/SportsCar.java
@@ -1,5 +1,12 @@
 package com.company;
 
 public class SportsCar extends  RentalCars{
+    boolean manualGear;
+    int horsePower;
 
+    public SportsCar(String b, String m, String fuel, int r, String rd, int o, boolean mGear, int hp){
+        super(b, m, fuel, r, rd, o);
+        manualGear = mGear;
+        horsePower = hp;
+    }
 }
