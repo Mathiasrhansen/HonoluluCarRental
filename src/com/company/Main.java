5ba39b8c41b99e35605917bb9bcf0e24760e3008
Mathiasrhansen <math86s0@stud.kea.dk>
2021-11-10 12:39:21 +0100

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,16 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws FileNotFoundException {
         ArrayList<RentalCars> carList = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+        //Scanner read = new Scanner(new File("Cars.txt"));
 
         SportsCar porsche = new SportsCar("Porsche", "911", "Petrol", "GW40958", "20/05-2020", 3500, true, 420);
         carList.add(porsche);
@@ -14,7 +19,20 @@ public class Main {
         FamilyCar ford = new FamilyCar("Ford", "S-Max", "Diesel", "CV17345", "16/11-2020", 28425, false, true, true, 7);
         carList.add(ford);
 
-        for (RentalCars rentalCars : carList) {
+        System.out.println("-------------------------------------");
+        System.out.println("|                                   |");
+        System.out.println("|        Honolulu Car Rental        |");
+        System.out.println("|                                   |");
+        System.out.println("-------------------------------------\n");
+        System.out.println("Main menu:\n");
+        System.out.println("Press 1 to");
+
+
+        }
+
+    public static void printMyList(ArrayList<RentalCars> list){
+        System.out.println("\n------------------------------------\n");
+        for (RentalCars rentalCars : list) {
             System.out.println(rentalCars + "\n\n------------------------------------\n");
         }
     }
