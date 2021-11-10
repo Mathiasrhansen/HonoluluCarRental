8e4939e7f0377b9339c38b8b6af1b5b9af911f65
Mathiasrhansen <math86s0@stud.kea.dk>
2021-11-10 11:30:37 +0100

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,5 +11,9 @@ public class Main {
         carList.add(porsche);
         FamilyCar nissan = new FamilyCar("Nissan", "Quest", "Petrol", "HF38663", "13/08-2017", 11000, false, true, false, 7);
         carList.add(nissan);
+
+        for (int i = 0; i < carList.size(); i++){
+            System.out.println(carList.get(i) + "\n\n******************\n");
+        }
     }
 }
